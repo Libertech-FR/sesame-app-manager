2e4695b86ad0ab6a3de4a902cf8ebc542c2c9f80
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2024-03-27 15:07:27 +0100

    Mise à jour de la page d'accueil avec une barre d'outils et un menu dynamique

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,58 @@
 <template lang="pug">
-  q-page
-    span Accueil
+q-page
+  q-toolbar
+    q-toolbar-title Accueil
+  .row
+    .col-4(v-for="item in menu").flex.flex-center.q-pa-md
+      q-btn(tile @click="push(item.path)" :color="item.color" style="width: 100%;")
+        q-icon(:name="item.icon" left size="42px")
+        span {{ item.label }}
 </template>
 
 <script lang="ts" setup>
+const router = useRouter()
+
+function push(path: string) {
+  router.push(path)
+}
+
+const menu = ref([
+  {
+    icon: 'mdi-account',
+    label: 'Liste des identités',
+    path: '/identities',
+    color: 'primary'
+  },
+  {
+    icon: 'mdi-card-account-details',
+    label: 'Liste des agents',
+    path: '/account',
+    color: 'primary'
+  },
+  {
+    icon: 'mdi-account-check',
+    label: 'A valider',
+    path: '/identities?sort[metadata.lastUpdatedAt]=desc&skip=0&limit=10&filters[@state][]=1',
+    color: 'primary'
+  },
+  {
+    icon: 'mdi-account-alert',
+    label: 'A compléter',
+    path: '/identities?sort[metadata.lastUpdatedAt]=desc&skip=0&limit=10&filters[@state][]=-2',
+    color: 'primary'
+  },
+  {
+    icon: 'mdi-account-remove',
+    label: 'En erreur',
+    path: '/identities?sort[metadata.lastUpdatedAt]=desc&skip=0&limit=10&filters[@state][]=-3',
+    color: 'primary'
+  },
+  {
+    icon: 'mdi-cog',
+    label: 'Paramètres',
+    path: '/settings',
+    color: 'primary'
+  }
+])
 
 </script>
