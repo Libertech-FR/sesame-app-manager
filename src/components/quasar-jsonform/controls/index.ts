72768188c572e11bbdee719236343080ca5cde5f
Tacx <12997062+tacxou@users.noreply.github.com>
2024-06-03 15:23:10 +0200

    Refactor code to use optional chaining for accessing nested properties

--- src/components/quasar-jsonform/controls/index.ts
+++ src/components/quasar-jsonform/controls/index.ts
@@ -1,26 +1,38 @@
 import QStringControlRenderer from "./QStringControlRenderer.vue";
+import QArrayControlRenderer from "./QArrayControlRenderer.vue";
 import QDateControlRenderer from "./QDateControlRenderer.vue";
 import QNumberControlRenderer from "./QNumberControlRenderer.vue";
-import { isControl, isDateControl, isNumberControl, rankWith, type JsonFormsRendererRegistryEntry } from "@jsonforms/core";
+import QPasswordControlRenderer from "./QPasswordControlRenderer.vue";
+import { isControl, isDateControl, isNumberControl, isPrimitiveArrayControl, rankWith, type JsonFormsRendererRegistryEntry } from "@jsonforms/core";
 
 const QDateControlRendererEntry: JsonFormsRendererRegistryEntry = {
-    renderer: QDateControlRenderer,
-    tester: rankWith(2, isDateControl),
+  renderer: QDateControlRenderer,
+  tester: rankWith(2, isDateControl),
 };
 
 const QNumberControlRendererEntry: JsonFormsRendererRegistryEntry = {
-    renderer: QNumberControlRenderer,
-    tester: rankWith(1, isNumberControl),
+  renderer: QNumberControlRenderer,
+  tester: rankWith(1, isNumberControl),
 };
-
 
 export const QStringControlRendererEntry: JsonFormsRendererRegistryEntry = {
   renderer: QStringControlRenderer,
   tester: rankWith(1, isControl),
 };
 
+export const QArrayControlRendererEntry: JsonFormsRendererRegistryEntry = {
+  renderer: QArrayControlRenderer,
+  tester: rankWith(2, isPrimitiveArrayControl),
+};
+
+export const QPasswordControlRendererEntry: JsonFormsRendererRegistryEntry = {
+  renderer: QPasswordControlRenderer,
+  tester: rankWith(2, isControl),
+};
+
 export const ControlsRenderer = [
-    QDateControlRendererEntry,
-    QNumberControlRendererEntry,
-    QStringControlRendererEntry,
+  QDateControlRendererEntry,
+  QNumberControlRendererEntry,
+  QStringControlRendererEntry,
+  QArrayControlRendererEntry,
 ];
