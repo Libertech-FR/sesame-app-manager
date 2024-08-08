cf4031dc73c2458e9a03f937a9cd64192da42851
Tacx <12997062+tacxou@users.noreply.github.com>
2024-08-08 12:31:57 +0200

    chore: Update identityState.ts to use optional chaining for data.value

--- src/stores/identityState.ts
+++ src/stores/identityState.ts
@@ -39,8 +39,8 @@ export const useIdentityStateStore = defineStore('identityStates', {
     },
     async fetchTotalCount() {
       try {
-        const { data } = await useHttp('/management/identities/count?limit=99999999');
-        this.total = data.value.data;
+        const { data } = await useHttp<any>('/management/identities/count?limit=99999999');
+        this.total = data.value?.data;
       } catch (error) {
         console.error(error);
       }
@@ -48,48 +48,48 @@ export const useIdentityStateStore = defineStore('identityStates', {
 
     async fetchToCompleteCount() {
       try {
-        const { data } = await useHttp(`/management/identities/count?filters[@state][]=${IdentityState.TO_COMPLETE}&limit=99999999`);
-        this[IdentityState.TO_COMPLETE] = data.value.data;
+        const { data } = await useHttp<any>(`/management/identities/count?filters[@state][]=${IdentityState.TO_COMPLETE}&limit=99999999`);
+        this[IdentityState.TO_COMPLETE] = data.value?.data;
       } catch (error) {
         console.error(error);
       }
     },
     async fetchToValidateCount() {
       try {
-        const { data } = await useHttp(`/management/identities/count?filters[@state][]=${IdentityState.TO_VALIDATE}&limit=99999999`);
-        this[IdentityState.TO_VALIDATE] = data.value.data;
+        const { data } = await useHttp<any>(`/management/identities/count?filters[@state][]=${IdentityState.TO_VALIDATE}&limit=99999999`);
+        this[IdentityState.TO_VALIDATE] = data.value?.data;
       } catch (error) {
         console.error(error);
       }
     },
     async fetchOnErrorCount() {
       try {
-        const { data } = await useHttp(`/management/identities/count?filters[@state][]=${IdentityState.ON_ERROR}&limit=99999999`);
-        this[IdentityState.ON_ERROR] = data.value.data;
+        const { data } = await useHttp<any>(`/management/identities/count?filters[@state][]=${IdentityState.ON_ERROR}&limit=99999999`);
+        this[IdentityState.ON_ERROR] = data.value?.data;
       } catch (error) {
         console.error(error);
       }
     },
     async fetchToSyncCount() {
       try {
-        const { data } = await useHttp(`/management/identities/count?filters[@state][]=${IdentityState.TO_SYNC}&limit=99999999`);
-        this[IdentityState.TO_SYNC] = data.value.data;
+        const { data } = await useHttp<any>(`/management/identities/count?filters[@state][]=${IdentityState.TO_SYNC}&limit=99999999`);
+        this[IdentityState.TO_SYNC] = data.value?.data;
       } catch (error) {
         console.error(error);
       }
     },
     async fetchProcessingCount() {
       try {
-        const { data } = await useHttp(`/management/identities/count?filters[@state][]=${IdentityState.PROCESSING}&limit=99999999`);
-        this[IdentityState.PROCESSING] = data.value.data;
+        const { data } = await useHttp<any>(`/management/identities/count?filters[@state][]=${IdentityState.PROCESSING}&limit=99999999`);
+        this[IdentityState.PROCESSING] = data.value?.data;
       } catch (error) {
         console.error(error);
       }
     },
     async fetchSyncedCount() {
       try {
-        const { data } = await useHttp(`/management/identities/count?filters[@state][]=${IdentityState.SYNCED}&limit=99999999`);
-        this[IdentityState.SYNCED] = data.value.data;
+        const { data } = await useHttp<any>(`/management/identities/count?filters[@state][]=${IdentityState.SYNCED}&limit=99999999`);
+        this[IdentityState.SYNCED] = data.value?.data;
       } catch (error) {
         console.error(error);
       }
