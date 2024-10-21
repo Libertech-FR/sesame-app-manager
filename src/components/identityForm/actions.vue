d749052e03c2cf579c441bb21f273012b9dddeb7
Alain Abbas <alain.abbas@libertech.fr>
2024-10-21 07:12:04 +0200

    function actif/inactif

--- src/components/identityForm/actions.vue
+++ src/components/identityForm/actions.vue
@@ -3,6 +3,7 @@ div.flex
   div
     q-btn(color="positive" icon='mdi-content-save-plus' @click="create" v-show="isNew" v-if="crud.create")
       q-tooltip.text-body2 Créer
+    q-toggle.q-pa-md.q-gutter-y-xl(v-if="props.identity?._id" @click="activate" color="green" left-label='Inactif' label="Actif" v-model="activation")
     q-btn.q-mx-xs(@click="sendInit" color="primary" icon="mdi-email-arrow-right"  :disabled="props.identity.state != IdentityState.SYNCED")
       q-tooltip.text-body2(slot="trigger") Envoyer le mail d'invitation
     q-btn.q-mx-xs(@click="submit" color="positive" icon="mdi-check"  v-show="!isNew" v-if="crud.update")
@@ -12,8 +13,6 @@ div.flex
       q-tooltip.text-body2(slot="trigger" v-else) L'état de l'identité ne permet pas de la synchroniser
     q-btn.q-mx-xs(v-if="props.identity?._id" @click="logs" color="grey-8" icon="mdi-file-document" :href="'/jobs?filters[:concernedTo.id]=' + props.identity?._id")
       q-tooltip.text-body2(slot="trigger") Voir les logs de l'identité
-  q-separator.q-mx-sm(vertical)
-  div
     q-btn.q-mx-xs(v-if="props.identity?._id" @click="deleteIdentity" :disabled="!props.identity.lastSync" color="negative" icon="mdi-delete")
       q-tooltip.text-body2(slot="trigger") Supprimer l'identité
 </template>
@@ -31,6 +30,7 @@ import { useErrorHandling } from '#imports'
 
 type IdentityResponse = operations['IdentitiesController_search']['responses']['200']['content']['application/json']
 type Identity = components['schemas']['IdentitiesDto']
+const activation=ref(true)
 const props = defineProps({
   identity: {
     type: Object as PropType<Identity>,
@@ -61,6 +61,44 @@ async function create() {
   // console.log('submit from actions')
   emits('create')
 }
+async function activate(){
+  let message=""
+  let bouton = ""
+  debugger
+  if (activation.value === false){
+    message="Voulez vous vraiment désactiver l'identité"
+    bouton="Désactiver"
+  }else{
+    message="Voulez vous vraiment activer l'identité"
+    bouton="Activer"
+  }
+
+  $q.dialog({
+    title: 'Confirmation',
+    message: message,
+    persistent: true,
+    ok: {
+      push: true,
+      color: 'positive',
+      label: bouton,
+    },
+    cancel: {
+      push: true,
+      color: 'negative',
+      label: 'Annuler',
+    },
+  }).onOk(async() => {
+    const requestOptions={method: 'POST',
+      body:JSON.stringify({id:props.identity._id,status:activation.value})}
+    const data=await $http.post('/management/identities/activation', requestOptions)
+      .catch(error => {
+        alert('There was an error!');
+      })
+  }).onCancel(() =>{
+    activation.value=activation.value === true? activation.value = false :activation.value = true
+  })
+
+}
 
 
 async function deleteIdentity() {
@@ -70,12 +108,12 @@ async function deleteIdentity() {
     persistent: true,
     ok: {
       push: true,
-      color: 'negative',
+      color: 'positive',
       label: 'Supprimer',
     },
     cancel: {
       push: true,
-      color: 'info',
+      color: 'negative',
       label: 'Annuler',
     },
   }).onOk(() => {
