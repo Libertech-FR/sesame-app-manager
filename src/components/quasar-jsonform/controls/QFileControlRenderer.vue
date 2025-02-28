5ad923a81f66d90b99cfd11c0f03dd75b5bf94a0
Tacx <12997062+tacxou@users.noreply.github.com>
2025-02-28 11:32:57 +0100

    Refactor QFileControlRenderer.vue for improved readability and consistency in code style

--- src/components/quasar-jsonform/controls/QFileControlRenderer.vue
+++ src/components/quasar-jsonform/controls/QFileControlRenderer.vue
@@ -23,14 +23,14 @@ div(style="cursor: pointer")
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 import { useFileDialog } from '@vueuse/core'
-import { rendererProps, useJsonFormsControl } from '@jsonforms/vue';
-import { isArray, isObject, isString, iterate } from 'radash';
-import type { RendererProps } from '@jsonforms/vue';
-import type { ControlElement } from '@jsonforms/core';
-import { useQuasarControl } from '../util';
-import { ControlWrapper } from '@jsonforms/vue-vanilla';
+import { rendererProps, useJsonFormsControl } from '@jsonforms/vue'
+import { isArray, isObject, isString, iterate } from 'radash'
+import type { RendererProps } from '@jsonforms/vue'
+import type { ControlElement } from '@jsonforms/core'
+import { useQuasarControl } from '../util'
+import { ControlWrapper } from '@jsonforms/vue-vanilla'
 
 const QStringControlRenderer = defineComponent({
   name: 'q-string-control-renderer',
@@ -42,63 +42,61 @@ const QStringControlRenderer = defineComponent({
   },
   inject: ['jsonforms'],
   setup(props: RendererProps<ControlElement>) {
-    const { files, open, reset, onChange: onChangeFile } = useFileDialog({
+    const {
+      files,
+      open,
+      reset,
+      onChange: onChangeFile,
+    } = useFileDialog({
       accept: 'image/*',
       multiple: false,
       directory: false,
     })
-    const config = useAppConfig();
+    const config = useAppConfig()
 
     return {
       open,
       onChangeFile,
       photo: null,
       baseUrl: config.baseUrl,
-      ...useQuasarControl(
-        useJsonFormsControl(props),
-        (value) => isObject(value) ? (value as any).value : value || undefined,
-      )
+      ...useQuasarControl(useJsonFormsControl(props), (value) => (isObject(value) ? (value as any).value : value || undefined)),
     }
   },
   methods: {
     onChangeControl(val) {
       console.log('val', val)
       debugger
-      this.onChange([
-        this.control?.uischema.options?.storage || 'data',
-        val.name,
-      ].join(':'));
+      this.onChange([this.control?.uischema.options?.storage || 'data', val.name].join(':'))
     },
   },
   async mounted() {
     this.onChangeFile((files) => {
-      this.onChangeControl(files?.length ? files[0] : null);
-    });
+      this.onChangeControl(files?.length ? files[0] : null)
+    })
   },
   computed: {
     computedLabel() {
-      return this.control.label === undefined ? this.control.schema.title : this.control.label;
+      return this.control.label === undefined ? this.control.schema.title : this.control.label
     },
     photoUrlQuery() {
       const auth = useAuth()
-      const query = new URLSearchParams();
-      const employeeNumber = (this.jsonforms as any)?.core?.data?.employeeNumber;
-      const employeeType = (this.jsonforms as any)?.core?.data?.employeeType;
+      const query = new URLSearchParams()
+      const employeeNumber = (this.jsonforms as any)?.core?.data?.employeeNumber
+      const employeeType = (this.jsonforms as any)?.core?.data?.employeeType
 
-      query.append('filters[:inetOrgPerson.employeeNumber]', employeeNumber);
-      query.append('filters[:inetOrgPerson.employeeType]', employeeType);
+      query.append('filters[:inetOrgPerson.employeeNumber]', isArray(employeeNumber) ? employeeNumber[0] : employeeNumber)
+      query.append('filters[:inetOrgPerson.employeeType]', employeeType)
 
-      if (auth.user?._id) query.append('id', `${auth.user?._id}`);
-      if (auth.user?.sseToken) query.append('key', `${auth.user?.sseToken}`);
+      if (auth.user?._id) query.append('id', `${auth.user?._id}`)
+      if (auth.user?.sseToken) query.append('key', `${auth.user?.sseToken}`)
 
       return {
         params: query.toString(),
         employeeNumber,
         employeeType,
-      };
+      }
     },
   },
-});
-export default QStringControlRenderer;
-
+})
+export default QStringControlRenderer
 </script>
