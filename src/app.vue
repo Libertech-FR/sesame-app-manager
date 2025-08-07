df05be83c329e058e7a5c7ebe11e6963352d91a3
Tacx <12997062+tacxou@users.noreply.github.com>
2025-08-07 16:14:42 +0200

    feat: implement dark mode handling and improve embedded page layout logic

--- src/app.vue
+++ src/app.vue
@@ -6,9 +6,17 @@
 </template>
 
 <script lang="ts" setup>
+const $q = useQuasar()
 const $route = useRoute()
 
 if (/1|yes|on|true/.test($route.query.embedded as string)) {
   setPageLayout('empty')
+}
+
+const darkMode = window.sessionStorage.getItem('darkMode')
+if (typeof darkMode === 'string') {
+  $q.dark.set(/true|1|on/.test(darkMode))
+} else {
+  window.sessionStorage.setItem('darkMode', $q.dark.isActive ? 'true' : 'false')
 }
 </script>
